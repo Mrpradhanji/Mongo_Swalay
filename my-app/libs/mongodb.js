6b8590c5a35965f26e095214d523b831db3e623f
Mrpradhanji <114905691+Mrpradhanji@users.noreply.github.com>
2024-07-02 16:22:03 +0530

    done schema

--- my-app/libs/mongodb.js
+++ my-app/libs/mongodb.js
@@ -1,17 +1,13 @@
 import mongoose from "mongoose";
 
- const connectMongoDB = async ()=>{
-    
+const connectMongoDB = async () => {
     try {
-       
-         await mongoose.connect(process.env.MONGODB_URI);
-        console.log("Connection Successfull");
-    } 
-    catch (error) {
-       
-        console.log("Unable to connect ")
+        await mongoose.connect(process.env.MONGODB_URI); 
+        console.log("Connection Successful");
+    } catch (error) {
+        
         console.log(error);
     }
-
 };
+
 export default connectMongoDB;
--- my-app/models/payments.js
+++ my-app/models/payments.js
@@ -1,7 +1,7 @@
 
-import mongoose,{Schema} from 'mongoose';
+import mongoose from 'mongoose';
 
-const PaymentSchema = new Schema({
+const PaymentSchema = new mongoose.Schema({
 
     id: {
         type :Number,
@@ -11,20 +11,24 @@ const PaymentSchema = new Schema({
             },
             message : (props)=>`${props.value} exceeds the 11 digit limit`
         },*/
-        required : [true, "Please provide a id"],
+        //required : [true, "Please provide a id"],
         unique : true,
+        required:true,
     },
     label : {
         type : String,
-        required : [true,"Enter payment label"],
+        default : null,
+        //required : [true,"Enter payment label"],
     },
     amount :{
         type : String,
-        required : [true,"Enter Amount"],
+        default : null,
+        //required : [true,"Enter Amount"],
 
     },
     status:{
         type : Number,
+        default : null,
         /*validate:{
             validator:function(value){
                 return value.toString().length<=11;
@@ -36,12 +40,13 @@ const PaymentSchema = new Schema({
     },
     time:{
         type:String,
-        required : [true,'Enter Time']
+        default : null,
+       // required : [true,'Enter Time']
     },
    
 });
 // if taken User then small all and pluralise the variable
-const Payment = mongoose.models.Payments || mongoose.model("Payment",PaymentSchema)
+const Payment = mongoose.models.Payment || mongoose.model("Payment",PaymentSchema);
 
 
 export default Payment;
